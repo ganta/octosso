ab6c10b357369cb9e28f9dd89c149e027148d463
Hideki Igarashi <hideki.develop@gmail.com>
2020-09-11 18:52:55 +0900

    chore: Install Prettier 2.1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint",
   ],
   rules: {},
 }
