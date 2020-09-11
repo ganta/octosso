50395916ff0d2aa7d2eb7df9999c5b99b5419e96
Hideki Igarashi <hideki.develop@gmail.com>
2020-09-11 18:45:10 +0900

    chore: Install ESLint 7.8.1

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    webextensions: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2019,
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: [
+    "@typescript-eslint",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  rules: {},
+}
